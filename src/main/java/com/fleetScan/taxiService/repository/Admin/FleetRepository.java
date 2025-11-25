5ad98a58035f2590c8710c864f5e344e52c6a81a
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-26 01:05:42 +0500

    Add FleetRepository interface with Spring Data JPA to manage fleet entities. Define custom query methods for finding by ID, name, public ID, and admin chat ID.

---
+++ src/main/java/com/fleetScan/taxiService/repository/Admin/FleetRepository.java
@@ -0,0 +1,18 @@
+package com.fleetScan.taxiService.repository.Admin;
+
+import com.fleetScan.taxiService.entity.Admin.Fleet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FleetRepository extends JpaRepository<Fleet, Long> {
+
+    Optional<Fleet> findById(Long id);
+    Optional<Fleet> findByName(String name);
+    Optional<Fleet> findByPublicId(String publicId);
+    Optional<Fleet> findByAdminChatId(Long adminChatId);
+
+    boolean existsByAdminChatId(Long adminChatId);
+    boolean existsByName(String name);
+
+}
