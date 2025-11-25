b61d53ea0525507997af8018e40ea6f251a57a38
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-26 01:07:27 +0500

    Add DriverRepository interface with Spring Data JPA to manage driver entities. Implement finders by ID, name, chat ID, and invite code for registration and authentication flows.

---
+++ src/main/java/com/fleetScan/taxiService/repository/Autopark/DriverRepository.java
@@ -0,0 +1,14 @@
+package com.fleetScan.taxiService.repository.Autopark;
+
+import com.fleetScan.taxiService.entity.Autopark.Driver.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+    Optional<Driver> findByDriverName(String driverName);
+    Optional<Driver> findByChatId(Long id);
+    Optional<Driver> findByInviteCode(String inviteCode);
+    Optional<Driver> findByChatId(String chatId);
+
+}
