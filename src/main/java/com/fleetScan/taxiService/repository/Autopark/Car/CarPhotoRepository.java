2c95847cd318aa77b29b5107c6098608843c1fb2
freedom_lovver <maksbuharskij16@gmail.com>
2026-04-22 16:03:51 +0500

    refactor: update car photo repository queries

--- src/main/java/com/fleetScan/taxiService/repository/Autopark/Car/CarPhotoRepository.java
+++ src/main/java/com/fleetScan/taxiService/repository/Autopark/Car/CarPhotoRepository.java
@@ -3,6 +3,12 @@ package com.fleetScan.taxiService.repository.autopark.car;
 import com.fleetScan.taxiService.domain.autopark.car.CarPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CarPhotoRepository extends JpaRepository <CarPhoto, Long> {
+    long countByDriverId(Long driverId);
+    List<CarPhoto> findAllByDriverIdOrderByCreatedAtDesc(Long driverId);
+    Optional<CarPhoto> findByIdAndDriverId(Long id, Long driverId);
 
 }
