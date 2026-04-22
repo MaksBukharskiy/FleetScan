96e37a2fb5d694db395e59b5368e426187831381
freedom_lovver <maksbuharskij16@gmail.com>
2026-04-22 16:04:05 +0500

    improve detected vehicle repository access

--- src/main/java/com/fleetScan/taxiService/repository/Autopark/vehicle/DetectedVehicleRepository.java
+++ src/main/java/com/fleetScan/taxiService/repository/Autopark/vehicle/DetectedVehicleRepository.java
@@ -3,17 +3,25 @@ package com.fleetScan.taxiService.repository.autopark.vehicle;
 import com.fleetScan.taxiService.domain.autopark.vehicle.DetectedVehicle;
 import com.fleetScan.taxiService.domain.autopark.vehicle.VehicleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface DetectedVehicleRepository extends JpaRepository<DetectedVehicle, Long> {
+public interface DetectedVehicleRepository extends JpaRepository<DetectedVehicle, Long>, JpaSpecificationExecutor<DetectedVehicle> {
     long countByStatus(VehicleStatus status);
+    long countBySourceChatId(Long sourceChatId);
     long countByDetectedAtBetween(LocalDateTime from, LocalDateTime to);
     long countByStatusAndDetectedAtBetween(VehicleStatus status, LocalDateTime from, LocalDateTime to);
     long countByPlateNumberAndDetectedAtBetween(String plateNumber, LocalDateTime from, LocalDateTime to);
 
     List<DetectedVehicle> findAllByDetectedAtBetweenOrderByDetectedAtDesc(LocalDateTime from, LocalDateTime to);
+    List<DetectedVehicle> findAllBySourceChatIdOrderByDetectedAtDesc(Long sourceChatId);
+    List<DetectedVehicle> findAllBySourceChatIdAndDetectedAtBetweenOrderByDetectedAtDesc(
+            Long sourceChatId,
+            LocalDateTime from,
+            LocalDateTime to
+    );
     List<DetectedVehicle> findAllByPlateNumberContainingIgnoreCaseAndDetectedAtBetweenOrderByDetectedAtDesc(
             String plateNumber,
             LocalDateTime from,
